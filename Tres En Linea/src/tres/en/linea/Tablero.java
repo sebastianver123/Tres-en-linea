76af5164dc1f1717050015034d4684c60595f451
estudiantes <estudiantes@labing00>
2017-03-08 17:58:03 -0500

    Base

---
+++ Tres En Linea/src/tres/en/linea/Tablero.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tres.en.linea;
+
+/**
+ *
+ * @author estudiantes
+ */
+public class Tablero {
+    
+    String[] letras=new String[9];
+    
+    
+}
